ed8114fee2ac29677ed9ce4afe122a28b05d33e1
tamlt2704 <tamlt2704@gmail.com>
2025-08-17 05:18:53 +0800

    add mmore test for individual and combination

--- test/models/payment_test.rb
+++ test/models/payment_test.rb
@@ -6,6 +6,7 @@ class PaymentTest < ActiveSupport::TestCase
 
   setup do
     travel_to Date.new(2025, 8, 16)
+    @company = Company.first
   end
 
   teardown do
@@ -31,5 +32,118 @@ class PaymentTest < ActiveSupport::TestCase
 
     assert_equal 10, payments_company_one.count
     assert_equal 5, payments_company_two.count
+  end
+
+  test "valid payment" do
+    payment = Payment.new(
+      company: @company,
+      employee_id: "emp999",
+      bank_bsb: "123456",
+      bank_account: "1234567",
+      amount_cents: 1000,
+      currency: "AUD",
+      pay_date: Date.today,
+      status: :pending
+    )
+    assert payment.valid?
+  end
+
+  test "invalid without required fields" do
+    payment = Payment.new
+    refute payment.valid?
+    assert_includes payment.errors[:employee_id], "can't be blank"
+    assert_includes payment.errors[:bank_bsb], "can't be blank"
+    assert_includes payment.errors[:bank_account], "can't be blank"
+    assert_includes payment.errors[:currency], "can't be blank"
+    assert_includes payment.errors[:pay_date], "can't be blank"
+  end
+
+  test "invalid amount_cents less than or equal to 0" do
+    payment = Payment.new(
+      company: @company,
+      employee_id: "emp999",
+      bank_bsb: "123456",
+      bank_account: "1234567",
+      amount_cents: 0,
+      currency: "AUD",
+      pay_date: Date.today
+    )
+    refute payment.valid?
+    assert_includes payment.errors[:amount_cents], "must be greater than 0"
+  end
+
+  test "invalid bank_bsb format" do
+    payment = Payment.new(
+      company: @company,
+      employee_id: "emp999",
+      bank_bsb: "12345",
+      bank_account: "1234567",
+      amount_cents: 1000,
+      currency: "AUD",
+      pay_date: Date.today
+    )
+    refute payment.valid?
+    assert_includes payment.errors[:bank_bsb], "BSB must be 6 digits"
+  end
+
+  test "invalid bank_account format" do
+    payment = Payment.new(
+      company: @company,
+      employee_id: "emp999",
+      bank_bsb: "123456",
+      bank_account: "12345",
+      amount_cents: 1000,
+      currency: "AUD",
+      pay_date: Date.today
+    )
+    refute payment.valid?
+    assert_includes payment.errors[:bank_account], "Account number must be 6–9 digits"
+  end
+
+  test "invalid currency" do
+    payment = Payment.new(
+      company: @company,
+      employee_id: "emp999",
+      bank_bsb: "123456",
+      bank_account: "1234567",
+      amount_cents: 1000,
+      currency: "USD",
+      pay_date: Date.today
+    )
+    refute payment.valid?
+    assert_includes payment.errors[:currency], "is not included in the list"
+  end
+
+  test "invalid pay_date in the past" do
+    payment = Payment.new(
+      company: @company,
+      employee_id: "emp999",
+      bank_bsb: "123456",
+      bank_account: "1234567",
+      amount_cents: 1000,
+      currency: "AUD",
+      pay_date: Date.yesterday
+    )
+    refute payment.valid?
+    assert_includes payment.errors[:pay_date], "must be greater than or equal to #{Date.today}"
+  end
+
+  test "combination of multiple invalid fields" do
+    payment = Payment.new(
+      company: @company,
+      employee_id: "",
+      bank_bsb: "abc123",
+      bank_account: "123",
+      amount_cents: -50,
+      currency: "EUR",
+      pay_date: Date.yesterday
+    )
+    refute payment.valid?
+    assert_includes payment.errors[:employee_id], "can't be blank"
+    assert_includes payment.errors[:bank_bsb], "BSB must be 6 digits"
+    assert_includes payment.errors[:bank_account], "Account number must be 6–9 digits"
+    assert_includes payment.errors[:amount_cents], "must be greater than 0"
+    assert_includes payment.errors[:currency], "is not included in the list"
+    assert_includes payment.errors[:pay_date], "must be greater than or equal to #{Date.today}"
   end
 end
